cd16730efa0a9bda77d94bc04d18d6f61f118f8f
inobu <inohkunobuya@gmail.com>
2020-11-10 20:56:59 +0900

    response系が配列になるのでStringで返却する

--- src/main/kotlin/org/practice/micronaut/bookshelf/presentation/book/BookResponse.kt
+++ src/main/kotlin/org/practice/micronaut/bookshelf/presentation/book/BookResponse.kt
@@ -1,16 +1,16 @@
 package org.practice.micronaut.bookshelf.presentation.book
 
 import org.practice.micronaut.bookshelf.application.book.BookDTO
-import java.time.LocalDate
+import java.time.format.DateTimeFormatter
 
-data class BookResponse(val id: String, val bookName: String?, val publicationDate: LocalDate, val authorName: String) {
+data class BookResponse(val id: String, val bookName: String?, val publicationDate: String, val authorName: String) {
     companion object {
         fun fromDTO(bookDTO: BookDTO): BookResponse {
             return bookDTO.run {
                 BookResponse(
                         id,
                         bookName.value,
-                        publicationDate.value,
+                        publicationDate.value.format(DateTimeFormatter.ISO_DATE),
                         authorName.value
                 )
             }
